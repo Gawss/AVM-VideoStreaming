5ce32035a8a9963c329bd466a607477181760011
Luis M. Palacio <luispalacio1997@hotmail.com>
2024-05-30 22:26:20 +0200

    VFX Imported

--- Assets/_WebRTC/Scripts/CopyTexture2RT.cs
+++ Assets/_WebRTC/Scripts/CopyTexture2RT.cs
@@ -1,18 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class CopyTexture2RT : MonoBehaviour
 {
+    [SerializeField] private RenderTexture targetRT;
+    [SerializeField] private RawImage rawImage;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
---
+++ Assets/_WebRTC/VFX/VFXPointCloudBinder.cs
@@ -0,0 +1,87 @@
+using UnityEngine;
+using UnityEngine.VFX;
+using UnityEngine.VFX.Utility;
+using UnityEngine.UI;
+
+namespace Akvfx
+{
+
+    [AddComponentMenu("VFX/Property Binders/Akvfx/Point Cloud Binder")]
+    [VFXBinder("Akvfx/Point Cloud")]
+    sealed class VFXPointCloudBinder : VFXBinderBase
+    {
+        #region VFX Binder Implementation
+
+        public string ColorMapProperty
+        {
+            get => (string)_colorMapProperty;
+            set => _colorMapProperty = value;
+        }
+
+        public string PositionMapProperty
+        {
+            get => (string)_positionMapProperty;
+            set => _positionMapProperty = value;
+        }
+
+        public string WidthProperty
+        {
+            get => (string)_widthProperty;
+            set => _widthProperty = value;
+        }
+
+        public string HeightProperty
+        {
+            get => (string)_heightProperty;
+            set => _heightProperty = value;
+        }
+
+        [VFXPropertyBinding("UnityEngine.Texture2D"), SerializeField]
+        ExposedProperty _colorMapProperty = "ColorMap";
+
+        [VFXPropertyBinding("UnityEngine.Texture2D"), SerializeField]
+        ExposedProperty _positionMapProperty = "PositionMap";
+
+        [VFXPropertyBinding("System.UInt32"), SerializeField]
+        ExposedProperty _widthProperty = "MapWidth";
+
+        [VFXPropertyBinding("System.UInt32"), SerializeField]
+        ExposedProperty _heightProperty = "MapHeight";
+
+        public int imageWidth;
+
+        public int imageHeight;
+
+        // public RenderTexture RT_colorMap;
+        // public RenderTexture RT_positionMap;
+
+        public RawImage IMG_colorMap;
+        public RawImage IMG_positionMap;
+
+        public string targetName;
+
+        public override bool IsValid(VisualEffect component)
+          => IMG_colorMap != null &&
+                component.HasTexture(_colorMapProperty) &&
+                component.HasTexture(_positionMapProperty) &&
+                component.HasUInt(_widthProperty) &&
+                component.HasUInt(_heightProperty);
+
+        public override void UpdateBinding(VisualEffect component)
+        {
+            if (IMG_colorMap == null) return;
+            if (IMG_positionMap == null) return;
+            component.SetTexture(_colorMapProperty, IMG_colorMap.texture);
+            component.SetTexture(_positionMapProperty, IMG_positionMap.texture);
+            component.SetUInt(_widthProperty, (uint)imageWidth);
+            component.SetUInt(_heightProperty, (uint)imageHeight);
+        }
+
+        public override string ToString()
+          => "Point Cloud : " +
+             $"'{_positionMapProperty}' -> {targetName ?? "(null)"}";
+
+        #endregion
+    }
+
+}
